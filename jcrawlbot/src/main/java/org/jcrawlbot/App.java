eb627381ce0b1709af6eacb5f992ff2626520097
mkirouac <mikedotnet@live.com>
2014-07-13 12:07:49 -0400

    Initial project commit

---
+++ jcrawlbot/src/main/java/org/jcrawlbot/App.java
@@ -0,0 +1,13 @@
+package org.jcrawlbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
